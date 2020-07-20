7a4bbcf5ee841527b7fa9223a26cfc773d54f8d9
Walter V. Faustine <waltervfaustine@gmail.com>
2020-07-20 12:42:58 +0300

    feat: 🎸 added support for API core model
    This commit will add support for API core model

---
+++ src/core/models/query-option.model.ts
@@ -0,0 +1,9 @@
+export interface QueryOption {
+    status:               string;
+    destinationDataSetId: string;
+    sourceDataElementId:  string;
+    sourceExchangePeriod: string;
+    sourceExchangeDate:   string;
+    sourceSystemId:       string;
+    destinationSystemId:  string;
+}
