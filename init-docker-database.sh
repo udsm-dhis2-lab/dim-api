a6becbd84a558183e999a9e504d4493a8f476326
Walter V. Faustine <waltervfaustine@gmail.com>
2020-07-30 01:12:15 +0300

    fix: 🐛 updated configurations

--- init-docker-database.sh
+++ init-docker-database.sh
@@ -1,2 +1,2 @@
-psql -h api-database -p 5434 -U postgres -c "CREATE DATABASE dimmediatordb;"
+psql -h postgres-database -p 5434 -U postgres -c "CREATE DATABASE dimmediatordb;"
 psql -h localhost -p 5434 -U postgres dimmediatordb < files/db/backup.sql
