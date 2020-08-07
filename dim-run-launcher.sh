d66d5cc554dc491981ab3d4fea9244a84f81dd12
Walter V. Faustine <waltervfaustine@gmail.com>
2020-08-07 17:04:28 +0300

    fix: 🐛 updated launcher configurations
    This commit will update launcher configurations

---
+++ dim-run-launcher.sh
@@ -0,0 +1,7 @@
+docker-compose -f  docker-compose-server.yml up
+docker exec -i dim-api_postgres-database_1 psql -U postgres -c "DROP DATABASE IF EXISTS epilabdb" 
+docker exec -i dim-api_postgres-database_1 psql -U postgres -c "CREATE DATABASE epilabdb WITH ENCODING='UTF8' OWNER=postgres;"
+docker exec -i dim-api_postgres-database_1 psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+docker container restart dim-api_postgres-database_1
+docker container restart dim-api_nginx_1
+docker container restart dim-api_api_1
