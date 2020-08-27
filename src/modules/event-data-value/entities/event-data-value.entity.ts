6b5a5128b12d75231cad9ddcc1a790a696159f1f
Walter V. Faustine <waltervfaustine@gmail.com>
2020-08-27 16:33:44 +0300

    fix: 🐛 updated event data value entity
    This commit will update event data value entity

--- src/modules/event-data-value/entities/event-data-value.entity.ts
+++ src/modules/event-data-value/entities/event-data-value.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn, BeforeInsert } from 'typeorm';
 import { EntityCoreProps } from 'src/core/entities/core-props';
 import { EventTracker } from 'src/modules/event/entities/event.entity';
 
@@ -25,9 +25,21 @@ export class EventDataValue extends EntityCoreProps {
         name: 'providedelsewhere',
         nullable: true,
     })
-    providedElsewhere: string;
+    providedElsewhere: boolean;
 
     @ManyToOne((type) => EventTracker, (eventTracker) => eventTracker.dataValues)
     @JoinColumn({ referencedColumnName: 'id' })
     event: EventTracker;
+
+    @BeforeInsert()
+    beforeInsertProvidedElsewhere() {
+        /**
+         *  You can generate UUID in different ways
+         *  1. You can generate uuid of any length: i.e uuid('', 20)
+         *      Example of UUID::: 8aydSxYBrrP
+         *  2. You can generat UUID by prepending a context specific keyword i.e uuid('DIMMediator', 20)
+         *      Example of UUID::: DIMMediator_8aydSxYBrrP
+         */
+        this.providedElsewhere = false;
+    }
 }
